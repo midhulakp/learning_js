eb58e84481ec5d8acac042ccc77edef8e3530fdc
midhulakp <115697790+midhulakp@users.noreply.github.com>
2024-11-13 16:03:45 +0000

    code changed

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1445533 // cannot change the value 
+let accountEmail = 'midhula@gmail.com'
+var accountPass = "midhula123"
+accountCity = "Palakad" 
+let accountState;
+
+// accountId = 2  // not allowed
+console.log(accountId);
+
+accountEmail="midhu@gmail.com"
+accountPass="123midhula"
+accountCity="bengaluru"
+
+console.log([accountId,accountEmail,accountPass,accountCity,accountState]);
+
+/*
+prefer not to use var
+because of issue in block scope and function scope
+
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+
+// alert("hello") // we are using node js not browser
+
+console.log(3  
+    + 3 );//code readability should be high
+ 
+
+    let name = "midhula" //string
+    let age = 23 // number
+    let isLoggedIn = true // boolean
+    let state; //undefined
+
+// primitive datatype
+
+    //number => 2 to power 53
+    //bigint 
+    //string => ""
+    //boolean => true or false
+    //null => standalone value,empty type -> object
+    //undefined => value not assigned type => undefined
+    //symbol  => unique
+
+    //object
+
+    console.log(typeof age); // show the type od data
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
+    
