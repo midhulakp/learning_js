131a34c3ac23ee5444ab7ce31c558200a558043e
midhulakp <115697790+midhulakp@users.noreply.github.com>
2024-11-15 16:15:31 +0000

    strings

---
+++ .devcontainer/01_basics/06_String.js
@@ -0,0 +1,38 @@
+const name = "midhula"
+
+const repoCount = 10
+
+// console.log(name + repoCount + " Value");
+
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
+const gameName = new String('Midhula-Frozen')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('F'));
+
+console.log(gameName.substring(0,4));
+
+console.log(gameName.slice(-13,4));
+
+const myname = "  midhula   "
+console.log(myname.trim());
+
+
+const url = "https://midhula.com/midhula%23pranav"
+
+console.log(url.replace('%23','-'));
+console.log(url.includes('midhula'));
+
+console.log(url.split('/'));
